99425723b8f94c632f9d301a164e50a94851f915
Arthur Passareli <arthurpamo23@gmail.com>
2026-06-04 19:44:33 -0400

    AtividadeFetchFlatList

--- src/App.tsx
+++ src/App.tsx
@@ -51,7 +51,15 @@ export default function App() {
         item.nome.toLowerCase().includes(marca.toLowerCase())
       );
 
-      setConsulta(marcasFiltradas);
+      setConsulta((prev) => {
+        const novasMarcas = marcasFiltradas.filter(
+          (marcaFiltrada) =>
+            !prev.some((item) => item.codigo === marcaFiltrada.codigo)
+        );
+
+        return [...prev, ...novasMarcas];
+      });
+
       setMarca("");
     } catch (e) {
       console.error(e);
@@ -63,20 +71,20 @@ export default function App() {
   return (
     <View style={styles.container}>
 
-      <Text style={styles.title}>Marcas de Veículos</Text>
+      <Text style={styles.title}>Marcas de Veículo</Text>
 
       <TextInput
         style={styles.input}
         value={marca}
         onChangeText={setMarca}
-        placeholder="Digite a marca do carro que deseja"
+        placeholder="Digite a marca do carro"
       />
 
       <TouchableOpacity onPress={handleFetchConsulta} style={styles.btn}>
         <Text style={styles.btnText}>Buscar marca</Text>
       </TouchableOpacity>
 
-      {loading && <Text style={styles.loading}>Carregando dados da marca...</Text>}
+      {loading && <Text style={styles.loading}>Carregando dados...</Text>}
 
       <FlatList
         data={consulta}
@@ -97,42 +105,51 @@ const styles = StyleSheet.create({
   container: {
     padding: 20,
     marginTop: 40,
+    backgroundColor: "#009739",
+    flex: 1,
   },
   title: {
     fontSize: 24,
     fontWeight: "bold",
     textAlign: "center",
     marginBottom: 20,
+    color: "#ffcc29",
   },
   input: {
-    backgroundColor: "#eee",
+    backgroundColor: "#ffffff",
     padding: 10,
     borderRadius: 8,
     marginBottom: 10,
+    borderWidth: 2,
+    borderColor: "#ffcc29",
   },
   btn: {
-    backgroundColor: "#4888e9",
+    backgroundColor: "#002776",
     padding: 12,
     borderRadius: 8,
     marginBottom: 20,
   },
   btnText: {
-    color: "#fff",
+    color: "#ffffff",
     textAlign: "center",
     fontWeight: "bold",
   },
   loading: {
     textAlign: "center",
     marginBottom: 15,
+    color: "#ffffff",
   },
   card: {
-    backgroundColor: "#eee",
+    backgroundColor: "#ffffff",
     padding: 12,
     borderRadius: 8,
     marginBottom: 10,
+    borderLeftWidth: 5,
+    borderLeftColor: "#ffcc29",
   },
   text: {
     fontSize: 16,
     marginTop: 6,
+    color: "#002776",
   },
 });
